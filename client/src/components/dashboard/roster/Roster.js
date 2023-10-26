2909a54d44d2b53d54eb44e6c4d54167a9ba849b
kfiddle <kenjfiddle@gmail.com>
2023-10-26 19:44:16 -0400

    rosterGenerator

--- client/src/components/dashboard/roster/Roster.js
+++ client/src/components/dashboard/roster/Roster.js
@@ -3,28 +3,81 @@ import { useSelector } from 'react-redux';
 
 import generalStore from '../../../contextStore/general-store';
 
+import RosterGenerator from '../../rosterGenerator/RosterGenerator';
+
+
 import styles from './Roster.module.css';
+
+const composerAndWorkUrl = 'daniels_query/by_composer_work';
+const workDetailsUrl = 'daniels_query/work_by_id';
 
 const Roster = () => {
   const { dashState } = useContext(generalStore);
   const { allInsts: insts } = useSelector((state) => state.insts);
 
+
+  const submitComposerAndWork = async () => {
+    const objToSend = { composer: 'Beethoven', work: 'Symphony No.5' };
+    console.log(insts)
+  
+    console.log(RosterGenerator(insts, '2222-2222', gig ));
+
+    // if (containsSymphony(work)) {
+    //   objToSend.work = `Symphony No.${returnNumber(work)}`;
+    // } else {
+    //   objToSend.work = work;
+    // }
+    const response = await fetch(process.env.REACT_APP_SERVER + composerAndWorkUrl, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(objToSend),
+    });
+
+    if (response.ok) {
+      let jsonified = await response.json();
+      console.log(jsonified);
+      // if (jsonified.length === 0) setBadSubmission(true);
+      // setRepliedWorks(jsonified);
+    }
+  };
+  // "719"
+
+  const submitWork = async (workId) => {
+    const reply = await fetch(process.env.REACT_APP_SERVER + workDetailsUrl, {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({ id: 719 }),
+    });
+    if (reply.ok) {
+      const workDetails = await reply.json();
+      console.log(workDetails.formula);
+    }
+  };
+
+
+
   let displayableChairs = [];
   if (dashState.chairs.length > 0) {
     let chairs = dashState.chairs.reduce((list, chair) => {
-      let instToFind = insts.find(inst => inst.id === chair.parts[0].inst);
+      let instToFind = insts.find((inst) => inst.id === chair.parts[0].inst);
       let instAbbv = instToFind.abbreviation;
       let chairId = chair.id;
       let rank = chair.parts[0].rank;
-      list.push({ instAbbv, rank, chairId});
+      list.push({ instAbbv, rank, chairId });
       return list;
-    }, [])
+    }, []);
 
-
-    displayableChairs = chairs.map((chair) => <div key={chair.chairId}>{chair.instAbbv} {chair.rank}</div>);
+    displayableChairs = chairs.map((chair) => (
+      <div key={chair.chairId}>
+        {chair.instAbbv} {chair.rank}
+      </div>
+    ));
   }
 
-  return <div>{displayableChairs}</div>;
+  return <div>{displayableChairs}
+  <button onClick={submitComposerAndWork}>TEST DANIELS</button>
+  <button onClick={submitWork}>TEST DANIELS SINGLE WORK</button>
+  </div>;
 };
 
 export default Roster;
---
+++ client/src/components/rosterGenerator/RosterGenerator.js
@@ -0,0 +1,129 @@
+// const chairSchema = new mongoose.Schema({
+//   player: { type: mongoose.Types.ObjectId, ref: 'Player' },
+//   gig: { type: mongoose.Types.ObjectId, ref: 'Gig', required: true },
+//   pieceNum: Number,
+//   parts: [
+//     {
+//       inst: { type: mongoose.Types.ObjectId, ref: 'Inst' },
+//       rank: Number,
+//     },
+//   ],
+// });
+
+const RosterGenerator = (insts, originalLibLine, gigId, pieceNum = 0) => {
+const libraryLine = originalLibLine.replace(/\s+/g, '');
+
+  class Part {
+    constructor(instId, rank) {
+      this.rank = rank ? +rank : null;
+      this.inst = instId;
+    }
+  }
+  class Chair {
+    constructor(part) {
+      this.gig = gigId;
+      this.pieceNum = pieceNum;
+      this.parts = [part];
+    }
+
+    add(part) {
+      this.parts.push(part);
+    }
+  }
+
+  const primaryNames = ['flute', 'oboe', 'clarinet', 'bassoon', 'horn', 'trumpet', 'trombone', 'tuba'];
+  const primaries = primaryNames.map((name) => insts.find((inst) => inst.name === name));
+
+  let isValid = true;
+  let chairsOnStage = [];
+
+  const instIdFromAbbrev = (abbrev) => {
+    const matchingInst = insts.find((inst) => inst.abbreviation === abbrev);
+    return matchingInst ? matchingInst.id : 0;
+  };
+
+  // ('3/cbn2');
+  const renderChairWithDoublings = (primaryInst, string) => {
+    let chair = new Chair(new Part(primaryInst.id, string[0]));
+    const partsArray = string.split('/');
+    const endsWithDigit = /\w\d$/;
+
+    for (let partString of partsArray.slice(1)) {
+      if (endsWithDigit.exec(partString)) {
+        const instId = instIdFromAbbrev(partString.slice(0, -1));
+        const rank = partString.slice(-1);
+        chair.add(new Part(instId, rank));
+      } else {
+        const instId = instIdFromAbbrev(partString);
+        chair.add(new Part(instId));
+      }
+    }
+
+    return chair;
+  };
+
+  const goBetweenBrackets = (j, index) => {
+    let primaryInst = primaries[index];
+    if (primaryInst === undefined) {
+      isValid = false;
+      return;
+    }
+    let bracketSlice = libraryLine.slice(j + 1);
+    let closingIndex = bracketSlice.indexOf(']');
+    if (closingIndex === -1) {
+      isValid = false;
+      return;
+    }
+    let withinBracketsScoreLines = bracketSlice.slice(1, closingIndex).split('.');
+
+    // by now, we will have only an array of [1, 2, 3/pic]
+    //     or, 3[1, 2, cbn], etc...
+    //4   [1, 2, 3/cbn2, cbn1]
+    // 4[1.2.3/pic2.pic1]  4[1.2.3.Eh]  4[1.2.3/Ebcl.bcl]  4[1.2.3/cbn2.cbn1] — 4  3  3  1 — tmp+4 — 3hp — cel, pf — str
+
+    withinBracketsScoreLines.forEach((scoreLine) => {
+      // in case the scoreline is an abbreviation only, like "cbn" or "pic";
+      const instFromAbbreviation = instIdFromAbbrev(scoreLine);
+      const fromAbbrevWithRank = instIdFromAbbrev(scoreLine.slice(0, -1));
+      if (instFromAbbreviation) chairsOnStage.push(new Chair(new Part(instFromAbbreviation.id)));
+      // if scoreline is just a number, make a chair from the primary inst and the number
+      else if (!isNaN(scoreLine)) {
+        let rank = scoreLine;
+        chairsOnStage.push(new Chair(new Part(primaryInst.id, rank)));
+
+        // in case scoreline ends with a number, like "cbn2", or "crnt3"
+      } else if (fromAbbrevWithRank && !isNaN(scoreLine.slice(-1))) {
+        const rank = scoreLine.slice(-1);
+        chairsOnStage.push(new Chair(new Part(fromAbbrevWithRank.id, rank)));
+
+        // in this case, scoreline would be something like "3/cbn2"
+      } else {
+        chairsOnStage.push(renderChairWithDoublings(primaryInst, scoreLine));
+      }
+    });
+    return closingIndex + j;
+  };
+
+  const mainLoop = (text) => {
+    let times = 0;
+    for (let j = 0; j < text.length; j++) {
+      if (text[j] !== '-') {
+        let nextChar = text[j + 1];
+        if (nextChar === '[') {
+          j = goBetweenBrackets(j, times);
+          times++;
+        } else if (!isNaN(text[j])) {
+          for (let k = 1; k <= text[j]; k++) {
+            chairsOnStage.push(new Chair(new Part(primaries[times].id, k)));
+          }
+          times++;
+        }
+      }
+    }
+  };
+
+  mainLoop(libraryLine);
+  return isValid ? chairsOnStage : false;
+};
+
+export default RosterGenerator;
