26444076e96a29ed285367c7fc973e8fd60684b8
EricDalio <ericdalio@gmail.com>
2023-11-01 13:07:04 -0400

    Fix Hamburger glitch

--- client/src/header/hamburger/HamburgerMenu.js
+++ client/src/header/hamburger/HamburgerMenu.js
@@ -10,30 +10,43 @@ const HamburgerMenu = ({ stripesHandler, sideBarOpen }) => {
     return () => setHovering(truFalse);
   };
 
+  //reset hover after dropdown click
+  const resetHover = () => {
+    return setHovering(false);
+  };
+
+  //FORMER DROPDOWN (X)
+  // const stripeMover = (stripeNum) => {
+  //   if (!sideBarOpen && !hovering) {
+  //     return styles[`bar${stripeNum}`];
+  //   } else if (sideBarOpen) {
+  //     return `${styles[`bar${stripeNum}`]} ${styles[`clickedBar${stripeNum}`]}`;
+  //   }
+  //   return `${styles[`bar${stripeNum}`]} ${styles[`hover${stripeNum}`]}`;
+  // };
+
   const stripeMover = (stripeNum) => {
-    if (!sideBarOpen && !hovering) {
+    if (!clicked && !hovering) {
       return styles[`bar${stripeNum}`];
-    } else if (sideBarOpen) {
-      return `${styles[`bar${stripeNum}`]} ${styles[`clickedBar${stripeNum}`]}`;
+    } else if (clicked && hovering) {
+      return `${styles[`bar${stripeNum}`]} ${styles[`hover${stripeNum}`]}`;
     }
     return `${styles[`bar${stripeNum}`]} ${styles[`hover${stripeNum}`]}`;
   };
 
   const clicker = () => {
     // stripesHandler();
-    console.log('clicked')
     // stripesHandler();
-    console.log('clicked 1', clicked);
     if (clicked === true) setClicked(false);
     if (clicked === false) {
       setClicked(true);
       setTimeout(() => {
         setClicked(false);
+        setHovering(false);
       }, 5000);
     }
-    console.log('clicked 2', clicked);
   };
- 
+
   return (
     <div>
       <div
@@ -42,7 +55,6 @@ const HamburgerMenu = ({ stripesHandler, sideBarOpen }) => {
         onMouseLeave={hovered(false)}
         onClick={clicker}
       >
-        {/* <div className={styles.centeringBox}> */}
         <div className={styles.centeringBox}>
           <span className={stripeMover(5)}></span>
           <span className={stripeMover(4)}></span>
@@ -51,7 +63,13 @@ const HamburgerMenu = ({ stripesHandler, sideBarOpen }) => {
           <span className={stripeMover(1)}></span>
         </div>
         <div>
-          {clicked && <Dropdown className={styles.dropdown} styles={styles} />}
+          {clicked && (
+            <Dropdown
+              className={styles.dropdown}
+              styles={styles}
+              resetHover={resetHover}
+            />
+          )}
         </div>
       </div>
     </div>
